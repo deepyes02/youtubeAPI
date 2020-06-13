5267e70d52ac71daafe5cffda72b4911d4823ed3
deepyes02 <deepyes@outlook.com>
2020-06-13 12:20:50 +0545

    barebones & dependencies axois, material ui

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//takes code from app
+import App from './App';
+
+//render the app in #root of html
+ReactDOM.render(<App/>, document.querySelector('#root'));
