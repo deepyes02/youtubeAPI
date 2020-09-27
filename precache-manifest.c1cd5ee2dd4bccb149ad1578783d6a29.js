55afc90d760bc5ecc5de1ecd15b5a996989ac7ba
deepyes02 <deepyes@outlook.com>
2020-09-27 13:10:11 +0545

    Updates

---
+++ precache-manifest.c1cd5ee2dd4bccb149ad1578783d6a29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6df0fc93f87e0e3c03b33afedb8a6819",
+    "url": "/youtubeAPI/index.html"
+  },
+  {
+    "revision": "cf9722af96beeb9368b9",
+    "url": "/youtubeAPI/static/css/main.cad15e32.chunk.css"
+  },
+  {
+    "revision": "503b2938f6b1a0f5a596",
+    "url": "/youtubeAPI/static/js/2.5e07f499.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/youtubeAPI/static/js/2.5e07f499.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf9722af96beeb9368b9",
+    "url": "/youtubeAPI/static/js/main.53246feb.chunk.js"
+  },
+  {
+    "revision": "2efec1f60e0b459f0d32",
+    "url": "/youtubeAPI/static/js/runtime-main.77207fb1.js"
+  }
+]);
